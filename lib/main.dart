8b242845ffe3b9327bb3861f5b609befa7510f81
gowrishvarma <gowrishvarma@gmail.com>
2023-12-19 12:43:40 +0530

    reopen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'package:image/image.dart' as imageLib;
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+// import 'package:pointycastle/pointycastle.dart';
+// import 'package:pointycastle/api.dart';
+// import 'dart:convert';
+// import 'package:pointycastle/api.dart';
+// import 'package:password_hash/password_hash.dart';
+
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:pointycastle/pointycastle.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,120 +21,137 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ximage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ximage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ximageState createState() => _ximageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ximageState extends State<ximage> {
+  List<List<int>>? pixelArray;
+  Uint8List? imageBytes; // Store the image bytes
+  MemoryImage? memoryImage; // Store the MemoryImage object
 
-  void _incrementCounter() {
+  // void _processImage() async {
+  //   final ByteData data = await rootBundle.load('assets/123.jpg');
+  //   final buffer = data.buffer.asUint8List();
+  //   final image = imageLib.decodeImage(Uint8List.fromList(buffer));
+
+  //   setState(() {
+  //     pixelArray = imageToPixelArray(image);
+  //   });
+  // }
+
+  void _processImage() async {
+    final ByteData data = await rootBundle.load('assets/X.jpg');
+    final buffer = data.buffer.asUint8List();
+    final image = imageLib.decodeImage(Uint8List.fromList(buffer));
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      pixelArray = imageToPixelArray(image);
+      // Convert the pixel array back to an image
+      // final image1 = pixelArrayToImage(pixelArray!);
+
+      // // Encode the image as bytes
+      // final List<int> pngBytes = imageLib.encodePng(image1);
+
+      // // Update imageBytes and memoryImage with the new image data
+      // imageBytes = Uint8List.fromList(pngBytes);
+      // memoryImage = MemoryImage(imageBytes!);
     });
+    Uint8List encryptionKey = Uint8List.fromList(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+    print(encryptPixelArray(pixelArray!, encryptionKey));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      child: Column(
+        children: [
+          // if (memoryImage != null)
+          //   Image(
+          //     image: memoryImage!,
+          //   ),
+          ButtonBar(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: TextButton(
+                  onPressed: () {
+                    _processImage(); // Call the function to process the image
+                  },
+                  child: Text("Do it!"),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  List<List<int>> imageToPixelArray(imageLib.Image? image) {
+    if (image == null) {
+      return [];
+    }
+
+    List<List<int>> result = [];
+    print(image.height);
+    print(image.width);
+    for (int i = 0; i < image.height; i++) {
+      List<int> row = [];
+      for (int j = 0; j < image.width; j++) {
+        row.add(image.getPixel(j, i));
+      }
+      result.add(row);
+    }
+    print(result);
+    // Convert the pixel array back to an image
+    imageLib.Image image1 = pixelArrayToImage(result);
+
+    return result;
+  }
+
+  List<List<int>> encryptPixelArray(
+      List<List<int>> pixelArray, Uint8List encryptionKey) {
+    final CipherParameters params = PaddedBlockCipherParameters(
+      ParametersWithIV(
+          KeyParameter(encryptionKey), Uint8List(16)), // IV is set to zero
+      null,
+    );
+
+    final BlockCipher encryptionCipher = PaddedBlockCipher("AES/CBC/PKCS7")
+      ..init(true, params);
+
+    return pixelArray.map((row) {
+      return row.map((pixelValue) {
+        final Uint8List input = Uint8List.fromList([pixelValue]);
+        final Uint8List encryptedData = encryptionCipher.process(input);
+        return encryptedData[0];
+      }).toList();
+    }).toList();
+  }
+
+  imageLib.Image pixelArrayToImage(List<List<int>> pixelArray) {
+    int height = pixelArray.length;
+    int width = pixelArray.isNotEmpty ? pixelArray[0].length : 0;
+
+    imageLib.Image image = imageLib.Image(width, height);
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        int pixelValue = pixelArray[y][x];
+        image.setPixel(x, y, pixelValue);
+      }
+    }
+    return image;
   }
 }
